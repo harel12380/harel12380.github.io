cd135626ee8984914e42012af884baf72888ecb7
harel12381 <harelhad200@gmail.com>
2023-06-06 23:32:38 +0300

    Remove try file

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 var animationActive = false;
-var image = document.getElementById('myImage');
+var image = document.getElementById("myImage");
 
 function toggleAnimation() {
   if (animationActive) return;
 
-  image.src = darkThemeToggle ? 'light-mode.gif' : 'dark-mode.gif';
+  image.src = darkThemeToggle ? "light-mode.gif" : "dark-mode.gif";
   animationActive = true;
   toggleTheme();
   setTimeout(function () {
@@ -17,43 +17,34 @@ var darkThemeToggle = false;
 
 function toggleTheme() {
   darkThemeToggle = !darkThemeToggle;
-
-  document.documentElement.style.colorScheme = darkThemeToggle
-  ? 'dark'
-  : 'light';
 }
 
-function setColorScheme(scheme) {
-  darkThemeToggle = scheme == 'dark';
-}
+const checkbox = document.getElementById("hidden-checkbox");
+const label = document.querySelector("label");
+const paragraphs = document.querySelectorAll("p");
+const headings1 = document.querySelectorAll("h1");
+const headings3 = document.querySelectorAll("h3");
+const links = document.querySelectorAll("a");
+const hobbies = document.querySelectorAll(".hobby");
+const skill = document.querySelectorAll(".skill");
+const portfolio = document.querySelectorAll(".portfolio");
 
-function getPreferredColorScheme() {
-  if (window.matchMedia) {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark';
-    } else {
-      return 'light';
-    }
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    paragraphs.forEach((paragraph) => paragraph.classList.add("covered"));
+    headings1.forEach((heading) => heading.classList.add("covered"));
+    headings3.forEach((heading) => heading.classList.add("covered"));
+    links.forEach((link) => link.classList.add("covered"));
+    hobbies.forEach((hobby) => hobby.classList.add("covered"));
+    skill.forEach((skill) => skill.classList.add("covered"));
+    portfolio.forEach((portfolio) => portfolio.classList.add("covered"));
+  }else {
+    paragraphs.forEach((paragraph) => paragraph.classList.remove("covered"));
+    headings1.forEach((heading) => heading.classList.remove("covered"));
+    headings3.forEach((heading) => heading.classList.remove("covered"));
+    links.forEach((link) => link.classList.remove("covered"));
+    hobbies.forEach((hobby) => hobby.classList.remove("covered"));
+    skill.forEach((skill) => skill.classList.remove("covered"));
+    portfolio.forEach((portfolio) => portfolio.classList.remove("covered"));
   }
-  return 'light';
-}
-
-function updateColorScheme() {
-  setColorScheme(getPreferredColorScheme());
-  updateStaticThemeImage();
-
-  document.documentElement.style.colorScheme = darkThemeToggle
-  ? 'dark'
-  : 'light';
-}
-
-function updateStaticThemeImage() {
-  image.src = darkThemeToggle ? 'dark.png' : 'light.png';
-}
-
-if (window.matchMedia) {
-  var colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  colorSchemeQuery.addEventListener('change', updateColorScheme);
-}
-
-updateColorScheme();
+});
